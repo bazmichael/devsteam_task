2fa682f24116e9cc95b48a41a770b3514221fb48
bazmichael <mykhailo.bazyshyn@gmail.com>
2020-12-01 13:50:07 +0100

    Simple navigation to the image page

--- lib/widgets/images_grid_widget.dart
+++ lib/widgets/images_grid_widget.dart
@@ -1,4 +1,5 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:devsteam/models/photo_vm.dart';
 import 'package:devsteam/providers/images_provider.dart';
 import 'package:devsteam/widgets/image_grid_item.dart';
 import 'package:flutter/material.dart';
@@ -14,8 +15,29 @@ final imagesGridWidget = Container(
       itemCount: provider.images.length,
       itemBuilder: (context, i) => Padding(
         padding: const EdgeInsets.all(8.0),
-        child: imageGridItem(provider.images[i].thumbImage),
+        child: InkWell(
+            child: imageGridItem(provider.images[i].thumbImage),
+            onTap: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => ImagePage(provider.images[i])))),
       ),
     ),
   ),
 ));
+
+class ImagePage extends StatelessWidget {
+  final PhotoViewModel vm;
+
+  ImagePage(this.vm) : assert(vm != null);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CachedNetworkImage(imageUrl: vm.fullImage, fit: BoxFit.fill),
+      appBar: AppBar(
+        title: Text(vm?.title ?? 'image description'),
+      ),
+    );
+  }
+}
